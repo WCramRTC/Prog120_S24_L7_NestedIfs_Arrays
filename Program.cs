ae0aea1ac7e182a4853907254903bbb041ad484c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-04-23 16:19:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog120_S24_L7_NestedIfs_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
